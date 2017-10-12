3bca612330ef14c1a962bda4c7899cdead07165d
haruglory9 <bisp123456@gmail.com>
2017-10-11 21:53:25 -0300

    C_one

---
+++ temp/producto.php
@@ -0,0 +1,256 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Pollution Network</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+    <!-- Plugin CSS -->
+    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+        <link href="css/creative.css" rel="stylesheet">
+    <style type="text/css">
+    .bg-sec {
+  background-color: #F2F2F2 !important; }
+
+    </style>
+
+  </head>
+
+  <body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">Pollution Network</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="http://35.195.21.237/nosotros.php">Acerca de Nosotros</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="http://35.195.21.237/mapas.php">Galería de Fotos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="http://35.195.21.237">Contacto</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#">Producto</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <header class="masthead">
+      <div class="header-content">
+        <div class="header-content-inner">
+          <h1 id="homeHeading">La idea que revolucionará Valdivia</h1>
+          <hr>
+                <a class="btn btn-primary btn-xl js-scroll-trigger" href="http://35.195.21.237/nosotros.php">Acerca de Nosotros</a>
+        </div>
+      </div>
+    </header>
+
+    <section class="bg-primary" id="about">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto text-center">
+            <h2 class="section-heading text-white">Todo lo que una ciudad inteligente necesita!</h2>
+            <hr class="light">
+            <p class="text-faded">Una ciudad no puede llamarse inteligente si no está desarrollada y pensada para el bienestar de sus propios habitantes, es por ello que nuestro reto es caracterizar la contaminación para una mejor toma de desiciones, ven y únete a nuestra red!</p>
+            <a class="btn btn-default btn-xl js-scroll-trigger" href="http://35.195.21.237">Conocenos!</a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="bg-sec" id="video">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto text-center">
+            <h2 class="section-heading">Conoce nuestra idea</h2>
+            <hr class="primary">
+            <iframe width="700" height="394" src="https://www.youtube.com/embed/0weVkq-FDSE" frameborder="0" allowfullscreen></iframe>
+          </div>
+        </div>
+  
+      </div>
+    </section>
+
+
+    <section id="services">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h2 class="section-heading">A vuestro servicio</h2>
+            <hr class="primary">
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box">
+              <i class="fa fa-4x fa-diamond text-primary sr-icons"></i>
+              <h3>Tú vales</h3>
+              <p class="text-muted">Para nuestra red colaborativa, lo más preciado eres tú</p>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box">
+              <i class="fa fa-4x fa-paper-plane text-primary sr-icons"></i>
+              <h3>Sé el primero</h3>
+              <p class="text-muted">solicita tu dispositivo, para que tú mismo puedas contribuir a medir la contaminación!</p>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box">
+              <i class="fa fa-4x fa-newspaper-o text-primary sr-icons"></i>
+              <h3>Mantente al día</h3>
+              <p class="text-muted">Suscribete a nuestra página de facebook para conocer las novedades</p>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box">
+              <i class="fa fa-4x fa-heart text-primary sr-icons"></i>
+              <h3>Cuida el Medio Ambiente</h3>
+              <p class="text-muted">Con tu contribución tendremos una ciudad más inteligente</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="p-0" id="portfolio">
+      <div class="container-fluid">
+        <div class="row no-gutter popup-gallery">
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="https://scontent.fccp2-1.fna.fbcdn.net/v/t1.0-9/21687737_10214130777521955_4047094272972889885_n.jpg?oh=d2d582c4fb6561c57d69684cbe3f9539&oe=5A535EF6">
+              <img class="img-fluid" src="https://scontent.fccp2-1.fna.fbcdn.net/v/t1.0-9/21687737_10214130777521955_4047094272972889885_n.jpg?oh=d2d582c4fb6561c57d69684cbe3f9539&oe=5A535EF6" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="https://scontent.fccp2-1.fna.fbcdn.net/v/t31.0-8/21688235_10214130822843088_2716266567759333148_o.jpg?oh=08cce3e69ddef823c66b71c226a1d1ea&oe=5A506DE7">
+              <img class="img-fluid" src="https://scontent.fccp2-1.fna.fbcdn.net/v/t31.0-8/21688235_10214130822843088_2716266567759333148_o.jpg?oh=08cce3e69ddef823c66b71c226a1d1ea&oe=5A506DE7" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="https://scontent.fccp2-1.fna.fbcdn.net/v/t1.0-9/21766655_10214130777561956_6916311655162549799_n.jpg?oh=bd5b8e9a36cb71a28d82d8fa25ef8b84&oe=5A50F7C2">
+              <img class="img-fluid" src="https://scontent.fccp2-1.fna.fbcdn.net/v/t1.0-9/21766655_10214130777561956_6916311655162549799_n.jpg?oh=bd5b8e9a36cb71a28d82d8fa25ef8b84&oe=5A50F7C2" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="https://scontent.fccp2-1.fna.fbcdn.net/v/t1.0-9/21687995_10214130778201972_332553211453042803_n.jpg?oh=3b9ba6a0f0989619f77c2dda81f2a2b7&oe=5A3AAE25">
+              <img class="img-fluid" src="https://scontent.fccp2-1.fna.fbcdn.net/v/t1.0-9/21687995_10214130778201972_332553211453042803_n.jpg?oh=3b9ba6a0f0989619f77c2dda81f2a2b7&oe=5A3AAE25" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="https://scontent.fccp2-1.fna.fbcdn.net/v/t31.0-8/21768923_10214130815202897_2125258682827384433_o.jpg?oh=e2edae52407af0540d5e8ca40ec7ee6b&oe=5A5F598E">
+              <img class="img-fluid" src="https://scontent.fccp2-1.fna.fbcdn.net/v/t31.0-8/21768923_10214130815202897_2125258682827384433_o.jpg?oh=e2edae52407af0540d5e8ca40ec7ee6b&oe=5A5F598E" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="https://scontent.fccp2-1.fna.fbcdn.net/v/t1.0-9/21686446_10214130778481979_9138841818198065446_n.jpg?oh=6e3a96feb878b1e93755df9c684403e4&oe=5A39B4D4">
+              <img class="img-fluid" src="https://scontent.fccp2-1.fna.fbcdn.net/v/t1.0-9/21686446_10214130778481979_9138841818198065446_n.jpg?oh=6e3a96feb878b1e93755df9c684403e4&oe=5A39B4D4" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <div class="call-to-action bg-dark">
+      <div class="container text-center">
+        <h2>Solicita tu Dispositivo e-Pollution GRATIS!</h2>
+        <a class="btn btn-default btn-xl sr-button" href="#">Solicitar aquí!</a>
+      </div>
+    </div>
+
+    <section id="contact">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto text-center">
+            <h2 class="section-heading">Tienes una idea nueva, BIENVENIDO!</h2>
+            <hr class="primary">
+            <p>Todas las nuevas ideas serán atendidas y consideradas, no dudes en contactarnos por cualquier mejora</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-4 ml-auto text-center">
+            <i class="fa fa-phone fa-3x sr-contact"></i>
+            <p>+56 9 632 222 111</p>
+          </div>
+          <div class="col-lg-4 mr-auto text-center">
+            <i class="fa fa-envelope-o fa-3x sr-contact"></i>
+            <p>
+              <a href="mailto:your-email@your-domain.com">contacto@pollutionnetwork.cl</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/popper/popper.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="vendor/scrollreveal/scrollreveal.min.js"></script>
+    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/creative.min.js"></script>
+
+  </body>
+
+</html>
+
